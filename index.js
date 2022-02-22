923f4094b3ffc5745fd1da7ece860a8ae12b9701
PointlessVenture <aiden.tracy@mymail.champlain.edu>
2022-02-22 13:32:37 -0500

    Resource Class

---
+++ index.js
@@ -0,0 +1,15 @@
+class resources
+{
+    StudentHappiness = 0;
+    StudentSanity = 0;
+    StudentEngagement = 0;
+
+    TeacherHappiness = 0;
+    TeacherSanity = 0;
+    TeacherEngagement = 0;
+
+    KnowledgeImparted = 0;
+    BloodAlchoholContent = 0;
+}
+
+
