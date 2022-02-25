4a1357c0b7e11fd49171721a8a38211440147646
Louis Vinton <louis.vinton@mymail.champlain.edu>
2022-02-24 20:24:39 -0500

    added stats button

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 
-export class resources
+class resources
 {
-    StudentHappiness = 50;
-    StudentSanity = 50;
-    StudentEngagement = 50;
+    constructor(baseStats) {
+        this.StudentHappiness = baseStats;
+        this.StudentSanity = baseStats;
+        this. StudentEngagement = baseStats;
 
-    TeacherHappiness = 50;
-    TeacherSanity = 50;
-    TeacherEngagement = 50;
+        this.TeacherHappiness = baseStats;
+        this.TeacherSanity = baseStats;
+        this.TeacherEngagement = baseStats;
 
-    KnowledgeImparted = 50;
-    BloodAlchoholContent = 0;
+        this.KnowledgeImparted = baseStats;
+        this.BloodAlchoholContent = 0;
+    }
 }
 
-export class Character {
+let gameStats = new resources(50);
+
+class Character {
     constructor(name, studentHappyOffset = 1, studentEngageOffset = 1, studentSanityOffset = 1, profHappyOffset = 1, profEngageOffset = 1, profSanityOffset = 1) {
         this.name = name;
         this.studentHappyOffset = studentHappyOffset;
@@ -23,6 +27,24 @@ export class Character {
         this.profEngageOffset = profEngageOffset;
         this.profSanityOffset = profSanityOffset;
     }
+}
+
+const loadStats = (event) => {
+    let stats = document.getElementById("stats")
+    console.log("clicked")
+
+    console.log(gameStats.StudentEngagement)
+
+    stats.innerHTML = 
+          "<p>Student Happiness = " + gameStats.StudentHappiness + "</p>"
+        + "<p>Student Sanity = " + gameStats.StudentSanity + "</p>"
+        + "<p>Student Engagement = " + gameStats.StudentEngagement + "</p>"
+        + "<p>Teacher Happiness = " + gameStats.TeacherHappiness + "</p>"
+        + "<p>Teacher Sanity = " + gameStats.TeacherSanity + "</p>"
+        + "<p>Teacher Engagement = " + gameStats.TeacherEngagement + "</p>"
+
+        + "<p>Knowledge Imparted = " + gameStats.KnowledgeImparted + "</p>"
+        + "<p>BAC = " + gameStats.BloodAlchoholContent + "</p>"
 }
 
 // Example Character
