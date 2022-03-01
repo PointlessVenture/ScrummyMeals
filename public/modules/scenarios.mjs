a15f7520595cf745e1d38aac24ffa7e64c895a88
PointlessVenture <aiden.tracy@mymail.champlain.edu>
2022-03-01 14:15:48 -0500

    Scenarios second pass

--- public/modules/scenarios.mjs
+++ public/modules/scenarios.mjs
@@ -3,9 +3,8 @@ import Character from "character.mjs"
 
 export default class Scenario
 {
-    constructor(OptionCount, Scene, Options, OptionOutputs)
+    constructor(Scene, Options, OptionOutputs)
     {
-        this.OptionCount = OptionCount;
         this.Scene = Scene;
         this.Options = Options;
         this.OptionOutputs = OptionOutputs;
@@ -17,14 +16,26 @@ var OptionOutput1;
 var OptionOutput2;
 var OptionOutput3;
 var Options;
-var OptionOutputs;
+var Option1Outputs;
+var Option2Outputs;
+var Option3Outputs;
+var AllOptionOutputs;
+
 
 SceneString = "While you're lecturing, a student interrupts you to ask a completely reasonable and thoughtful question. They sound genuinely confused."
 Options = ["berate", "readSlides", "explain"]
 OptionOutput1 = "You viciously berate the student for thier foolish questions, and insinuate that they bring shame upon thier ancestors."
+OptionOutput2 = "You berate the student, in a sarcastic, yet slightly charming way that makes them laugh nervously."
+OptionOutput3 = "You berate the student in a way that gives the vibe that you aren't upset, just really, really dissapointed."
+Option1Outputs = [OptionOutput1, OptionOutput2, OptionOutput3]
+OptionOutput1 = "You go back a few slides to re-read a portion from earlier, answering the student's question."
 OptionOutput2 = "You go back a few slides to re-read a portion from earlier, answering the student's question."
+OptionOutput3 = "You go back a few slides to re-read a portion from earlier, answering the student's question."
+Option2Outputs = [OptionOutput1, OptionOutput2, OptionOutput3]
+OptionOutput1 = "You take the time to explain the answer to the question and make sure the class is on the same page."
+OptionOutput2 = "You take the time to explain the answer to the question and make sure the class is on the same page."
 OptionOutput3 = "You take the time to explain the answer to the question and make sure the class is on the same page."
-OptionOutputs = [OptionOutput1, OptionOutput2, OptionOutput3]
+Option3Outputs = [OptionOutput1, OptionOutput2, OptionOutput3]
+AllOptionOutputs = [Option1Outputs, Option2Outputs, Option3Outputs]
 
-
-export const StudentDecentQuestion = new Scenario(3,SceneString, Options, OptionOutputs)
+export const StudentDecentQuestion = new Scenario(SceneString, Options, OptionOutputs)
