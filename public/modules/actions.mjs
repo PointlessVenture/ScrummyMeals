c1416e8b771a036e158f0e70b4cb8ab4bde867c9
collin-westgate <collin.westgate@mymail.champlain.edu>
2022-03-03 16:42:31 -0500

    Added more actions, four major groups. Let me know if we think we need more/what else!

--- public/modules/actions.mjs
+++ public/modules/actions.mjs
@@ -23,7 +23,7 @@ const explainSlides = (resource, char) =>
 }
 
 
-// Student Responses
+// Student Question
 const explain = (resource, char) =>
 {
     resource.StudentSanity += 10  * char.studentSanityOffset;
@@ -48,12 +48,14 @@ const reiterate = (resource, char) =>
 
 const readNotes = (resource, char) =>
 {
-    
+    resource.TeacherEngagement += 10 * char.profEngageOffset;
+	resource.TeacherHappiness -= 5 * (1 / char.profHappyOffset);
 }
 
 const helpStudents = (resource, char) =>
 {
-    
+    resource.StudentHappiness += 5 * char.studentHappyOffset;
+	resource.StudentSanity += 10 * char.studentSanityOffset;
 }
 
 const relax = (resource, char) =>
@@ -68,6 +70,28 @@ const drink = (resource, char) =>
 	resource.TeacherSanity += 10 * char.profSanityOffset;
 	resource.BloodAlchoholContent += 0.04;
 }
+
+
+// Set up groups
+const allowAnyone = (resource, char) =>
+{
+	resource.StudentHappiness += 10 * char.studentHappyOffset;
+	resource.StudentEngagement -= 5 * (1 / char.studentEngageOffset);
+}
+
+const assignGroups = (resource, char) =>
+{
+	resource.StudentEngagement += 5 * char.studentHappyOffset;
+	resource.studentSanityOffset -= 5 * (1 / char.studentEngageOffset);
+}
+
+const splitClass = (resource, char) =>
+{
+	resource.StudentHappiness += 5 * char.studentHappyOffset;
+	resource.StudentEngagement -= 10 * (1 / char.studentEngageOffset);
+	resource.StudentSanity += 5 * char.studentSanityOffset;
+}
+
 
 export {berate, readSlides, skimSlides, explain}
 
