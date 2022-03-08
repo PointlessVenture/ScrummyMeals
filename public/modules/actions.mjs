4ce3ab1e48c3d0e66cca442c9516d1194c52adeb
Westgate <collin.westgate@mymail.champlain.edu>
2022-03-08 13:23:44 -0500

    Coming up with actions

--- public/modules/actions.mjs
+++ public/modules/actions.mjs
@@ -94,6 +94,52 @@ const splitClass = (resource, char) =>
 }
 
 
+// Losing interest
+
+const joke = (resource, char) =>
+{
+
+}
+
+const getAttention = (resource, char) =>
+{
+
+}
+
+const ignore = (resource, char) =>
+{
+
+}
+
+const yell = (resource, char) =>
+{
+
+}
+
+
+// Explain a concept
+
+const tangent = (resource, char) =>
+{
+
+}
+
+const explainNormal = (resource, char) =>
+{
+
+}
+
+const basicExplanation = (resource, char) =>
+{
+
+}
+
+const inDepthExplanation = (resource, char) =>
+{
+
+}
+
+
 export {berate, readSlides, skimSlides, explain, explainSlides, reiterate, readNotes, helpStudents, relax, drink, allowAnyone, assignGroups, splitClass}
 
 window.berate = berate;
